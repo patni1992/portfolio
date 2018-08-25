ba925f7d68e6605b6bca4e8673f4ba01c8f47f8b
patriknilsson1992@gmail.com <patriknilsson1992@gmail.com>
2018-08-26 00:15:12 +0200

    add contact form

--- client/js/home.js
+++ client/js/home.js
@@ -1,5 +1,22 @@
 import Terminal from "./terminal";
-
 const terminal = new Terminal();
+const emailForm = document.getElementById("email-form");
+const sendBtn = document.getElementById("send-btn");
 
 terminal.typeWriter(document.getElementById("demo"), "> Patrik Nilsson");
+
+emailForm.addEventListener("submit", e => {
+  e.preventDefault();
+  sendBtn.classList.add("is-loading");
+  const data = new URLSearchParams();
+  for (const pair of new FormData(emailForm)) {
+    data.append(pair[0], pair[1]);
+  }
+  fetch("/test", {
+    method: "POST",
+    body: data
+  }).then(response => {
+    sendBtn.classList.remove("is-loading");
+    console.log(response);
+  });
+});
