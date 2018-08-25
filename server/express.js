5d7723c6f594fa91a880d71faed674bacb6482ef
patriknilsson1992@gmail.com <patriknilsson1992@gmail.com>
2018-08-26 00:15:38 +0200

    add body-parser, add test route for mail

--- server/express.js
+++ server/express.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 
 const webpack = require("webpack");
+const bodyParser = require("body-parser");
 const config = require("../webpack.js");
 const server = express();
 const compiler = webpack(config);
@@ -28,8 +29,18 @@ server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
 
+server.use(bodyParser.urlencoded({ extended: false }));
+
+// parse serverlication/json
+server.use(bodyParser.json());
+
 server.get("/cv", function(req, res) {
   var filePath = path.join(__dirname, "../dist/images/cv-2018-pn-921113.pdf");
 
   res.download(filePath);
+});
+
+server.post("/test", function(req, res) {
+  console.log(req.body);
+  res.send("ok");
 });
