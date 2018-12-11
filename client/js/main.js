0f0f1d49c75afcd1cc496785a2ba03bbf48d1da2
Patrik <patriknilsson1992@gmail.com>
2018-12-11 14:46:19 +0100

    update mini css extract plugin, move flickity css

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,7 @@
 
 if (process.env.NODE_ENV !== "production") {require("webpack-hot-middleware/client?reload=true")}
-
-import "../../node_modules/flickity/dist/flickity.css"
-import "../../node_modules/flickity-fullscreen/fullscreen.css"
+import "../sass/main.scss";
 import "flickity";
 import "flickity-fullscreen";
-import "../sass/main.scss";
 import "../index.hbs";
 import "./home";
